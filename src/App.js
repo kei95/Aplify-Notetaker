471b3e453b2384b1a05c2288d298a8748418fe49
Kei95 <kei0613shine.star@gmail.com>
2020-06-28 16:06:41 -0700

    Add mutation on the page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import { API, graphqlOperation } from "aws-amplify";
+import { withAuthenticator } from "aws-amplify-react";
+
+import { createNote } from "./graphql/mutations";
 
 function App() {
-  return <div>App</div>;
+  const [notes] = useState([]);
+  const [note, setNote] = useState({});
+
+  const handleAddNote = (e) => {
+    console.log(e);
+    e.preventDefault();
+    API.graphql(graphqlOperation(createNote, { input: note }));
+  };
+
+  return (
+    <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
+      <h1 className="code f2-1">Amplify Notetaker</h1>
+      <form action="mb3" onSubmit={(e) => handleAddNote(e)}>
+        <input
+          className="pa2 f4"
+          placeholder="Write your note"
+          onChange={(e) => setNote({ note: e.target.value })}
+        />
+        <button className="pa2 f4" type="submit">
+          Add Note
+        </button>
+      </form>
+      <div>
+        {notes.map((item) => (
+          <div key={item.id} className="flex items-center">
+            <li className="list pa1 f3">{item.note}</li>
+            <button className="bg-transparent bn f4">
+              <span>&times;</span>
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default App;
+export default withAuthenticator(App, { includeGreetings: true });
--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import Amplify from "aws-amplify";
 import aws_exports from "./aws-exports";
+import "@aws-amplify/ui/dist/style.css";
 Amplify.configure(aws_exports);
+
+//bewpfdswzhvrdvddlc@ttirv.org
 
 ReactDOM.render(
   <React.StrictMode>
